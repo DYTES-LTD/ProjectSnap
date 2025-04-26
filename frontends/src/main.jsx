e488a1ccd745184eaa70be5cd215544237a52ce6
shahan <johndoe@email.com>
2025-04-27 00:34:22 +0530

    code Structure updated along some ui changes

--- frontends/src/main.jsx
+++ frontends/src/main.jsx
@@ -3,9 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import React from 'react'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
+import Userhome from './Components/Userhome.jsx'
+import store from './store/store.js'
+import { Provider } from 'react-redux'
+
+const router=createBrowserRouter([{
+  path:'/',
+  element:<App/>,
+  children:[
+   {
+    index:true,
+    element:<Userhome/>
+   }
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <Provider store={store}>
+<RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
---
+++ frontends/src/pages/Homepage.jsx
@@ -0,0 +1,19 @@
+  import React from 'react'
+  import Userhome from '../Components/Userhome';
+  import '../Components/CssFiles/home.css'
+
+
+
+  function Homepage() {
+    
+  
+
+  
+    return (
+      <div className='home w-screen h-screen overflow-x-hidden'>
+        <Userhome />
+      </div>
+    )
+  }
+
+  export default Homepage
