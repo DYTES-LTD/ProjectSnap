e11543e8dbbfe2b07338aa1612067f91cf1ac1b2
shahan <johndoe@email.com>
2025-04-26 14:50:10 +0530

    final Push

--- frontends/src/App.jsx
+++ frontends/src/App.jsx
@@ -1,12 +1,19 @@
-import Home from "./pages/Home";
+import { Provider } from "react-redux";
+import Userhome from "./Components1/Userhome";
+import Home from "./Components1/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Store } from "lucide-react";
+import store from "./store/store";
+import React from "react";
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
+      <Provider store={store}>
+      <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+      </Provider>
       </BrowserRouter>
     </>
   );
